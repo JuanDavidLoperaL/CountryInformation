d2f25a4371a7e3e751c07e26f565d0f08e5ce01e
Juan David Lopera <juandavidl2011.jdll@gmail.com>
2020-02-29 14:44:46 -0300

    App initialization

---
+++ Countries/Countries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Countries
+//
+//  Created by Juan David Lopera Lopez on 2/29/20.
+//  Copyright © 2020 Juan David Lopera Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
